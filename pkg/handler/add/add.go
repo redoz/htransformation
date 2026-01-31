f5ddea0730402e66456fa8b38204e5976c8cff16
Patrik Husfloen <redoz@redoz.com>
2026-01-31 16:38:40 +0100

    feat: add HeaderPrefix support to Add handler
    Enable Add handler to reference other header values using HeaderPrefix,
    consistent with Join handler behavior. This allows copying values from
    existing headers when adding new ones.
    
    Example: Value: '^X-Forwarded-For' with HeaderPrefix: '^' will read
    the value from the X-Forwarded-For header.

--- pkg/handler/add/add.go
+++ pkg/handler/add/add.go
@@ -2,6 +2,7 @@ package add
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/tomMoulard/htransformation/pkg/types"
 	"github.com/tomMoulard/htransformation/pkg/utils/header"
@@ -24,11 +25,39 @@ func (a *Add) Validate() error {
 }
 
 func (a *Add) Handle(rw http.ResponseWriter, req *http.Request) {
+	value := getValue(a.rule.Value, a.rule.HeaderPrefix, req)
+
 	if a.rule.SetOnResponse {
-		rw.Header().Add(a.rule.Header, a.rule.Value)
+		rw.Header().Add(a.rule.Header, value)
 
 		return
 	}
 
-	header.Add(req, a.rule.Header, a.rule.Value)
+	header.Add(req, a.rule.Header, value)
+}
+
+// getValue checks if prefix exists, the given prefix is present,
+// and then proceeds to read the existing header (after stripping the prefix)
+// to return as value.
+func getValue(ruleValue, valueIsHeaderPrefix string, req *http.Request) string {
+	actualValue := ruleValue
+
+	if valueIsHeaderPrefix != "" && strings.HasPrefix(ruleValue, valueIsHeaderPrefix) {
+		header := strings.TrimPrefix(ruleValue, valueIsHeaderPrefix)
+		// If the resulting value after removing the prefix is empty,
+		// we return the actual value,
+		// which is the prefix itself.
+		// This is because doing a req.Header.Get("") would not fly well.
+		if header == "" {
+			return actualValue
+		}
+
+		if strings.EqualFold(header, "Host") {
+			actualValue = req.Host
+		} else {
+			actualValue = req.Header.Get(header)
+		}
+	}
+
+	return actualValue
 }
